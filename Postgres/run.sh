4de0021c4465ed68a2b7ae28cf9a06fd7cce8b72
Ekaterina <stolbovaya@yahoo.com>
2022-01-05 11:30:49 +0300

    port out

--- Postgres/run.sh
+++ Postgres/run.sh
@@ -1,4 +1,6 @@
-kubectl create -f ./postgres-pv.yaml
-kubectl create -f ./postgres-pvc.yaml
-kubectl apply -f ./postgres-creds.yaml
+kubectl create -f ./postgres-pv.yml
+kubectl create -f ./postgres-pvc.yml
+kubectl apply -f ./postgres-creds.yml
 kubectl apply -f ./postgres-deployment.yaml
+kubectl port-forward --address localhost,192.168.1.6 deployments/postgres 5432:5432
+
